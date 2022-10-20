ff2db51da3df0dff07c59dd8ebddfd201e97feab
s0600204 <dev@s0600204>
2022-10-20 16:43:35 +0100

    Refactor the parsing
    This refactors into one method that looks at each text block and determines
    what node it should be, then passes it to an appropriate method that does the
    actual conversion.

--- pyqt5_rst/qt2doctree.py
+++ pyqt5_rst/qt2doctree.py
@@ -7,6 +7,14 @@ from docutils import (
 
 from PyQt5.QtGui import QFont
 
+
+class SkipText(Exception):
+    """Skip appending a block's text."""
+
+class NextIteration(Exception):
+    """Continue with next Iteration."""
+
+
 class Qt2Doctree:
 
     def __init__(self):
@@ -16,65 +24,49 @@ class Qt2Doctree:
 
         self._list_stack = []
 
-    def convert(self, qt5_document):
-        doctree = utils.new_document("", frontend.get_default_settings())
-
         # In Qt5, it's possible to indent more than once.
-        indentation_stack = [0]
+        self._indentation_stack = [0]
 
-        self._section_stack = [doctree]
-        self._section_level_stack = [0]
+    @property
+    def current_node(self):
+        return self._section_stack[-1][-1]
 
-        block = qt5_document.firstBlock()
-        while block.isValid():
-            block_format = block.blockFormat()
-            level = block_format.headingLevel()
-            block_indent = block.blockFormat().indent()
-            disable_node_text_target_append = False
+    @property
+    def current_section(self):
+        return self._section_stack[-1]
 
-            if block_indent > indentation_stack[-1]:
-                # This is either a block quote or a literal block
-                format_list = block.textFormats()
-                if len(format_list) > 1 or not format_list[0].format.font().family() == 'monospace':
-                    block_quote = nodes.block_quote()
-                    self._section_stack[-1].append(block_quote)
-                    self._section_stack.append(block_quote)
-                    indentation_stack.append(block_indent)
-                    continue
+    def append_node(self, node):
+        self.current_section.append(node)
 
-                if isinstance(self._section_stack[-1][-1], nodes.literal_block):
-                    # Continuation of a literal block
-                    node_text_target = self._section_stack[-1][-1]
-                    indent = '  ' * (block_indent - 1)
-                    node_text_target.append(nodes.Text('\n%s' % indent))
-                    disable_node_text_target_append = True
-                else:
-                    node_text_target = nodes.literal_block()
+    def append_section(self, node):
+        self._section_stack.append(node)
 
-            else:
+    def append_text(self, text, text_format=None):
+        if text_format:
+            text_node = self.format_text_segment(text, text_format)
+        else:
+            text_node = nodes.Text(text)
 
-                while block_indent < indentation_stack[-1]:
-                    self._section_stack.pop()
-                    indentation_stack.pop()
+        self.current_node.append(text_node)
 
-                if level > 0:
-                    # This block is a section header
-                    node_text_target = self.append_header(block)
+    def append_text_from_block(self, block):
+        text = block.text()
+        for segment in block.textFormats():
+            self.append_text(
+                text[segment.start:segment.start + segment.length],
+                segment.format)
 
-                else:
-                    node_text_target = nodes.paragraph()
+    def build_block_quote(self, block):
+        block_indent = block.blockFormat().indent()
+        block_quote = nodes.block_quote()
 
-            self.append_text_to_node(node_text_target, block)
-            if node_text_target.children and not disable_node_text_target_append:
-                # Don't append an empty node
-                self._section_stack[-1].append(node_text_target)
+        self.append_node(block_quote)
+        self.append_section(block_quote)
 
-            block = block.next()
+        self._indentation_stack.append(block_indent)
+        raise NextIteration
 
-        return doctree
-
-    def append_header(self, block):
-
+    def build_header(self, block):
         # Backtrack up the stack if it's a higher-level header
         header_level = block.blockFormat().headingLevel()
         while header_level <= self._section_level_stack[-1]:
@@ -84,20 +76,68 @@ class Qt2Doctree:
         # @todo: look up how `ids` and `names` should *actually* be created/formed
         text_lower = block.text().lower()
         new_section = nodes.section(ids=text_lower, names=text_lower)
-        self._section_stack[-1].append(new_section)
 
-        self._section_stack.append(new_section)
+        self.append_node(new_section)
+        self.append_section(new_section)
         self._section_level_stack.append(header_level)
 
-        return nodes.title()
+        self.append_node(nodes.title())
 
-    def append_text_to_node(self, node, block):
-        text = block.text()
-        for segment in block.textFormats():
-            node.append(
-                self.format_text_segment(
-                    text[segment.start:segment.start + segment.length],
-                    segment.format))
+    def build_literal_block(self, block):
+        current_node = self.current_node
+        if isinstance(current_node, nodes.literal_block):
+            # Continuation of a literal block
+            block_indent = block.blockFormat().indent()
+            indent = '  ' * (block_indent - 1)
+            self.append_text('\n%s' % indent)
+        else:
+            self.append_node(nodes.literal_block())
+
+    def convert(self, qt5_document):
+        doctree = utils.new_document("", frontend.get_default_settings())
+
+        self._section_stack = [doctree]
+        self._section_level_stack = [0]
+
+        block = qt5_document.firstBlock()
+        while block.isValid():
+            block_format = block.blockFormat()
+            level = block_format.headingLevel()
+            block_indent = block.blockFormat().indent()
+
+            try:
+                if block_indent > self._indentation_stack[-1]:
+                    # This is either a block quote or a literal block
+                    format_list = block.textFormats()
+                    if len(format_list) == 1 and format_list[0].format.font().family() == 'monospace':
+                        self.build_literal_block(block)
+                    else:
+                        self.build_block_quote(block)
+
+                else:
+
+                    while block_indent < self._indentation_stack[-1]:
+                        self._section_stack.pop()
+                        self._indentation_stack.pop()
+
+                    if level > 0:
+                        # This block is a section header
+                        self.build_header(block)
+
+                    else:
+                        self.append_node(nodes.paragraph())
+
+                self.append_text_from_block(block)
+
+            except NextIteration:
+                continue
+
+            except SkipText:
+                pass
+
+            block = block.next()
+
+        return doctree
 
     def format_text_segment(self, text_segment, text_format):
 
